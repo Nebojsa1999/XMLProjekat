de83bd734596f643d6917ea319006dfedb3cc418
Darijan Mićić <darijan.micic10@gmail.com>
2022-04-27 18:15:49 +0200

    feat: write main method for users microservice

--- backend/user_service/main.go
+++ backend/user_service/main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"github.com/Nebojsa1999/XMLProjekat/backend/user_service/database"
+	"github.com/Nebojsa1999/XMLProjekat/backend/user_service/routes"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
 
+func main() {
+	database.Connect()
+
+	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowCredentials: true,
+	}))
+
+	routes.Setup(app)
+
+	app.Listen(":8000")
 }
