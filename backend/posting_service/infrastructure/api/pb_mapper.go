1ee10a54608b17ae191948ee149b6f1304e580c2
“DujinA” <dujinaleksandar@gmail.com>
2022-05-06 22:09:54 +0200

    protobuf mapper za posting_service

--- backend/posting_service/infrastructure/api/pb_mapper.go
+++ backend/posting_service/infrastructure/api/pb_mapper.go
@@ -1,1 +1,72 @@
 package api
+
+
+import (
+	pb "github.com/Nebojsa1999/XMLProjekat/backend/common/proto/posting_service"
+	"github.com/Nebojsa1999/XMLProjekat/backend/posting_service/domain"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func mapPost(post *domain.Post) *pb.Post {
+	postPb := &pb.Post {
+		Id:	 post.Id.Hex(),
+		Content: post.Content,
+		Image: post.Image,
+		LikesCount: post.LikesCount,
+		DislikesCount: post.DislikesCount,
+		Link: post.Link
+	}
+
+	for _, Comment := range post.Comments {
+		postPb.Comments = append(postPb.Comments, &pb.Comment {
+			Code: comment.Code,
+			Content: comment.Content
+		})
+	}
+
+	return postPb
+}
+
+func mapPostRequest(postPb *pb.Post) *domain.Post {
+	id, _ := primitive.ObjectIDFromHex(postPb.Id)
+	ownerId, _ := primitive.ObjectIDFromHex(postPb.OwnerId)
+	Post := &domain.Post{
+		Id: id,
+		OwnerId: ownerId,
+		Conent: postPb.Conent,
+		Image: postPb.Image,
+		LikesCount: postPb.LikesCount,
+		DislikesCount: postPb.DislikesCount,
+		Comments: make([]domain.Post, 0),
+		Link: postPb.Link,
+	}
+
+	for _, commentPb := range postPb.Comments {
+		comment := domain.Comment {
+			Code: commentPb.Code,
+			Content: commentPb.Content
+		}
+
+		Post.Comments = append(Post.Comments, comment)
+	}
+
+	return Post
+}
+
+func mapComment(commentPb *domain.Comment) *pb.Comment {
+	Comment := &pb.Comment{
+		Code: commentPb.Code,
+		Content: commentPb.Content
+	}
+
+	return Comment
+}
+
+func mapCommentOnPostRequest(commentPb *pb.Comment) *domain.Comment {
+	Comment := &domain.Comment{
+		Code: commentPb.Code,
+		Content: commentPb.Content
+	}
+
+	return Comment
+}
