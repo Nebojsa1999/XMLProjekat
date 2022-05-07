34347d61e56d399a22a3fccada233a3514c28355
“DujinA” <dujinaleksandar@gmail.com>
2022-05-07 23:42:46 +0200

    server.Start() in main.go

--- backend/posting_service/main.go
+++ backend/posting_service/main.go
@@ -1,1 +1,12 @@
 package main
+
+import (
+	"github.com/Nebojsa1999/XMLProjekat/backend/posting_service/startup"
+	cfg "github.com/Nebojsa1999/XMLProjekat/backend/posting_service/startup/config"
+)
+
+func main() {
+	config := cfg.NewConfig()
+	server := startup.NewServer(config)
+	server.Start()
+}
