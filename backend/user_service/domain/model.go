4add5834ed9fe8f4fa1b7da3ecf7488d7fac660b
Darijan Mićić <darijan.micic10@gmail.com>
2022-05-07 20:29:25 +0200

    style: beautify struct "User" in domain

--- backend/user_service/domain/model.go
+++ backend/user_service/domain/model.go
@@ -13,18 +13,18 @@ const (
 )
 
 type User struct {
-	Id primitive.ObjectID `json:"id" bson:"_id"`
-	Username string `json:"username" bson:"username"`
-	Password string `json:"-" bson:"password"`
-	IsPrivate bool `json:"is_private" bson:"is_private"`
-	FirstName string `json:"first_name" bson:"first_name"`
-	LastName string `json:"last_name" bson:"last_name"`
-	Email string `json:"email" bson:"email"`
-	Gender Gender `json:"gender" bson:"gender"`
-	DateOfBirth time.Time `json:"date_of_birth" bson:"date_of_birth"`
-	Biography string `json:"biography" bson:"biography"`
-	WorkExperience string `json:"work_experience" bson:"work_experience"`
-	Education string `json:"education" bson:"education"`
-	Skills string `json:"skills" bson:"skills"`
-	Interests string `json:"interests" bson:"interests"`
+	Id             primitive.ObjectID `json:"id" bson:"_id"`
+	Username       string             `json:"username" bson:"username"`
+	Password       string             `json:"password" bson:"password"`
+	IsPrivate      bool               `json:"is_private" bson:"is_private"`
+	FirstName      string             `json:"first_name" bson:"first_name"`
+	LastName       string             `json:"last_name" bson:"last_name"`
+	Email          string             `json:"email" bson:"email"`
+	Gender         Gender             `json:"gender" bson:"gender"`
+	DateOfBirth    time.Time          `json:"date_of_birth" bson:"date_of_birth"`
+	Biography      string             `json:"biography" bson:"biography"`
+	WorkExperience string             `json:"work_experience" bson:"work_experience"`
+	Education      string             `json:"education" bson:"education"`
+	Skills         string             `json:"skills" bson:"skills"`
+	Interests      string             `json:"interests" bson:"interests"`
 }
