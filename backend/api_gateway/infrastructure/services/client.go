a1507cc804c4d026b117206299ebde744419bbbb
Darijan Mićić <darijan.micic10@gmail.com>
2022-05-08 18:19:42 +0200

    feat: add connection to posting service in gateway

--- backend/api_gateway/infrastructure/services/client.go
+++ backend/api_gateway/infrastructure/services/client.go
@@ -1,6 +1,7 @@
 package services
 
 import (
+	postingPb "github.com/Nebojsa1999/XMLProjekat/backend/common/proto/posting_service"
 	userPb "github.com/Nebojsa1999/XMLProjekat/backend/common/proto/user_service"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
@@ -14,6 +15,15 @@ func NewUserClient(address string) userPb.UserServiceClient {
 	}
 
 	return userPb.NewUserServiceClient(conn)
+}
+
+func NewPostingClient(address string) postingPb.PostingServiceClient {
+	conn, err := getConnection(address)
+	if err != nil {
+		log.Fatalf("Failed to start gRPC connection to Posting service: %v", err)
+	}
+
+	return postingPb.NewPostingServiceClient(conn)
 }
 
 func getConnection(address string) (*grpc.ClientConn, error) {
--- backend/api_gateway/startup/config/config.go
+++ backend/api_gateway/startup/config/config.go
@@ -6,12 +6,16 @@ type Config struct {
 	Port     string
 	UserHost string
 	UserPort string
+	PostingHost string
+	PostingPort string
 }
 
 func NewConfig() *Config {
 	return &Config{
-		Port:          os.Getenv("GATEWAY_PORT"),
-		UserHost: os.Getenv("USER_SERVICE_HOST"),
-		UserPort: os.Getenv("USER_SERVICE_PORT"),
+		Port:        os.Getenv("GATEWAY_PORT"),
+		UserHost:    os.Getenv("USER_SERVICE_HOST"),
+		UserPort:    os.Getenv("USER_SERVICE_PORT"),
+		PostingHost: os.Getenv("POSTING_SERVICE_HOST"),
+		PostingPort: os.Getenv("POSTING_SERVICE_PORT"),
 	}
 }
