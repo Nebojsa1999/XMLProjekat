0db9a66f337f0c8a1abd52f9abf96e8e54132043
Darijan_Micic_RA_203_2017 <darijan.micic10@gmail.com>
2022-05-29 19:57:55 +0200

    feat: change user model & add more
    Models added: "Company" and "CompanyRegistrationRequest".

--- agent-app-backend/domain/model.go
+++ agent-app-backend/domain/model.go
@@ -8,9 +8,9 @@ import (
 type UserRole string
 
 const (
-	COMMON_USER   UserRole = "COMMON__USER"
-	COMPANY_OWNER          = "COMPANY_OWNER"
-	ADMINISTRATOR          = "ADMINISTRATOR"
+	CommonUser    UserRole = "CommonUser"
+	CompanyOwner           = "CompanyOwner"
+	Administrator          = "Administrator"
 )
 
 type Gender string
@@ -22,22 +22,21 @@ const (
 )
 
 type User struct {
-	Id                 primitive.ObjectID `json:"id" bson:"_id"`
-	Role               UserRole           `json:"role" bson:"role"`
-	Username           string             `json:"username" bson:"username"`
-	Password           string             `json:"password" bson:"password"`
-	FirstName          string             `json:"firstName" bson:"first_name"`
-	LastName           string             `json:"lastName" bson:"last_name"`
-	Email              string             `json:"email" bson:"email"`
-	Phone              string             `json:"phone" bson:"phone"`
-	Gender             Gender             `json:"gender" bson:"gender"`
-	DateOfBirth        time.Time          `json:"dateOfBirth" bson:"date_of_birth"`
-	Biography          string             `json:"biography" bson:"biography"`
-	WorkExperience     string             `json:"workExperience" bson:"work_experience"`
-	Education          string             `json:"education" bson:"education"`
-	Skills             string             `json:"skills" bson:"skills"`
-	Interests          string             `json:"interests" bson:"interests"`
-	CompanyDescription string             `json:"companyDescription" bson:"company_description"`
+	Id             primitive.ObjectID `json:"id" bson:"_id"`
+	Role           UserRole           `json:"role" bson:"role"`
+	Username       string             `json:"username" bson:"username"`
+	Password       string             `json:"password" bson:"password"`
+	FirstName      string             `json:"firstName" bson:"first_name"`
+	LastName       string             `json:"lastName" bson:"last_name"`
+	Email          string             `json:"email" bson:"email"`
+	Phone          string             `json:"phone" bson:"phone"`
+	Gender         Gender             `json:"gender" bson:"gender"`
+	DateOfBirth    time.Time          `json:"dateOfBirth" bson:"date_of_birth"`
+	Biography      string             `json:"biography" bson:"biography"`
+	WorkExperience string             `json:"workExperience" bson:"work_experience"`
+	Education      string             `json:"education" bson:"education"`
+	Skills         string             `json:"skills" bson:"skills"`
+	Interests      string             `json:"interests" bson:"interests"`
 }
 
 type Credentials struct {
@@ -47,4 +46,29 @@ type Credentials struct {
 
 type AgentAppToken struct {
 	Token string `json:"token"`
+}
+
+type Company struct {
+	Id          primitive.ObjectID `json:"id" bson:"_id"`
+	OwnerId     primitive.ObjectID `json:"ownerId" bson:"owner_id"`
+	Name        string             `json:"name" bson:"name"`
+	Address     string             `json:"address" bson:"address"`
+	Email       string             `json:"email" bson:"email"`
+	Phone       string             `json:"phone" bson:"phone"`
+	AreaOfWork  string             `json:"areaOfWork" bson:"area_of_work"`
+	Description string             `json:"description" bson:"description"`
+	WorkCulture string             `json:"workCulture" bson:"work_culture"`
+}
+
+type CompanyRegistrationRequestStatus string
+
+const (
+	Pending  CompanyRegistrationRequestStatus = "Pending"
+	Accepted                                  = "Accepted"
+	Rejected                                  = "Rejected"
+)
+
+type CompanyRegistrationRequest struct {
+	OwnerId primitive.ObjectID               `json:"ownerId" bson:"owner_id"`
+	Status  CompanyRegistrationRequestStatus `json:"status" bson:"status"`
 }
