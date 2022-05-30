79cd2a42f346532911fb0b9263051750a53d45f2
ra149 <fresh.trifunovic@gmail.com>
2022-05-30 10:56:52 +0200

    updated main bug fix

--- backend/job_service/main.go
+++ backend/job_service/main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"github.com/Nebojsa1999/XMLProjekat/backend/job_service/startup"
+	cfg "github.com/Nebojsa1999/XMLProjekat/backend/job_service/startup/config"
+)
+
 func main() {
 	config := cfg.NewConfig()
 	server := startup.NewServer(config)
