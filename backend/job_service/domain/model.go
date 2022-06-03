954ce30d545e91cb93082b85192a738d9fd85fad
Darijan Mićić <darijan.micic10@gmail.com>
2022-06-03 23:43:22 +0200

    improvement: field "createdat" of struct "job"
    Field "CreatedAt" of struct "Job" is now of type "time.Time".

--- backend/job_service/domain/model.go
+++ backend/job_service/domain/model.go
@@ -2,13 +2,14 @@ package domain
 
 import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
 )
 
 type Job struct {
-	Id           primitive.ObjectID `bson:"_id"`
-	UserId       primitive.ObjectID `bson:"userId"`
-	CreatedAt    primitive.DateTime `bson:"created_at"`
-	Position     string             `bson:"position"`
-	Description  string             `bson:"description"`
-	Requirements string             `bson:"requirements"`
+	Id           primitive.ObjectID `json:"id" bson:"_id"`
+	UserId       primitive.ObjectID `json:"userId" bson:"user_id"`
+	CreatedAt    time.Time          `json:"createdAt" bson:"created_at"`
+	Position     string             `json:"position" bson:"position"`
+	Description  string             `json:"description" bson:"description"`
+	Requirements string             `json:"requirements" bson:"requirements"`
 }
