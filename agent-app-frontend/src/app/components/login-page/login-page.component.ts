412801abc03863bc49cfa97b773e8c89819e6374
Darijan Mićić <darijan.micic10@gmail.com>
2022-06-03 18:39:43 +0200

    feat: write type script code for login page

--- agent-app-frontend/src/app/components/login-page/login-page.component.ts
+++ agent-app-frontend/src/app/components/login-page/login-page.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import jwt_decode from 'jwt-decode';
+
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 import { Credentials } from 'src/app/models/credentials';
 
@@ -14,14 +19,15 @@ export class LoginPageComponent implements OnInit {
 
   token: string = '';
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private authService: AuthService, private formBuilder: FormBuilder, 
+    private router: Router) { }
 
   ngOnInit(): void {
     this.form = this.formBuilder.group({
       username: ['', Validators.compose([Validators.required, Validators.minLength(4), 
-      Validators.maxLength(32)])],
+        Validators.maxLength(32)])],
       password: ['', Validators.compose([Validators.required, Validators.minLength(6), 
-      Validators.maxLength(32)])]
+        Validators.maxLength(32)])]
     });
   }
 
@@ -31,6 +37,37 @@ export class LoginPageComponent implements OnInit {
     const credentials: Credentials = {
       username: this.form.value.username,
       password: this.form.value.password
+    }
+
+    this.authService.loginWith(credentials).subscribe(
+      data => {
+        console.log('Login response: ', data);
+
+        localStorage.setItem('agentAppToken', data.accessToken);
+        let tokenInfo = this.getDecodedAccessToken(data.accessToken);
+
+        localStorage.setItem('id', tokenInfo.id);
+        localStorage.setItem('username', tokenInfo.username);
+        localStorage.setItem('role', tokenInfo.role);
+        localStorage.setItem('ownedCompanyId', tokenInfo.ownedCompanyId);
+        localStorage.setItem('issuedCompanyRequestId', tokenInfo.issuedCompanyRequestId);
+        localStorage.setItem('exp', tokenInfo.exp);
+
+        this.router.navigateByUrl('/').then(() => { window.location.reload(); });
+      },
+      error => {
+        this.isSubmitted = false;
+
+        console.log('Error on login: ', error);
+      }
+    );
+  }
+
+  getDecodedAccessToken(token: string): any {
+    try {
+      return jwt_decode(token);
+    } catch (error) {
+      return '';
     }
   }
 }
