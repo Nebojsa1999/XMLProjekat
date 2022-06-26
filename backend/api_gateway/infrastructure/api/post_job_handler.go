32fc512ff638d5ebbbe5fede7c6ccadc721b7ac5
Darijan Mićić <darijan.micic10@gmail.com>
2022-06-26 18:02:27 +0200

    feat: write handler for posting a job offer
    Handler lacks a method for the final step, creating a post itself. It contains all methods necessary before the final step: parsing user's id as a claim from API token, checking in user microservice whether user with that id has generated an API token; creating a job in job microservice.

---
+++ backend/api_gateway/infrastructure/api/post_job_handler.go
@@ -0,0 +1,145 @@
+package api
+
+import (
+	"context"
+	"fmt"
+	"github.com/Nebojsa1999/XMLProjekat/backend/api_gateway/domain"
+	"github.com/Nebojsa1999/XMLProjekat/backend/api_gateway/infrastructure/services"
+	jobPb "github.com/Nebojsa1999/XMLProjekat/backend/common/proto/job_service"
+	userPb "github.com/Nebojsa1999/XMLProjekat/backend/common/proto/user_service"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"log"
+	"net/http"
+	"os"
+)
+
+type PostJobHandler struct {
+	userClientAddress    string
+	jobClientAddress     string
+	postingClientAddress string
+}
+
+func NewPostJobHandler(userClientAddress, jobClientAddress, postingClientAddress string) Handler {
+	return &PostJobHandler{
+		userClientAddress:    userClientAddress,
+		jobClientAddress:     jobClientAddress,
+		postingClientAddress: postingClientAddress,
+	}
+}
+
+func (handler *PostJobHandler) Init(mux *runtime.ServeMux) {
+	err := mux.HandlePath("POST", "/post/job-offer", handler.PostAJobOffer)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+}
+
+func (handler *PostJobHandler) PostAJobOffer(writer http.ResponseWriter, request *http.Request, pathParams map[string]string) {
+	if !isContentTypeValid(writer, request) {
+		return
+	}
+
+	postJobOfferRequest, err := decodePostJobOfferRequestFromBody(request.Body)
+	if err != nil {
+		http.Error(writer, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	userId, err := handler.getDislinktUserIdClaimFromJobOffersAPIToken(postJobOfferRequest.JobOffersAPIToken)
+	if err != nil {
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	jobOffersAPITokenStatusRequest := &domain.JobOffersAPITokenStatusRequest{UserId: userId}
+	err = handler.hasUserGeneratedJobOffersAPIToken(jobOffersAPITokenStatusRequest)
+	if err != nil {
+		http.Error(writer, err.Error(), http.StatusNotFound)
+		return
+	}
+
+	if !jobOffersAPITokenStatusRequest.HasGeneratedToken {
+		http.Error(writer, "User did not generate API token.", http.StatusBadRequest)
+		return
+	}
+
+	err = handler.createJob(postJobOfferRequest.Job)
+	if err != nil {
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	/*
+	err = handler.createPostWithJobOffer(postJobOfferRequest.Job)
+	if err != nil {
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	*/
+
+	writer.WriteHeader(http.StatusCreated)
+	renderJSON(writer, "")
+}
+
+func (handler *PostJobHandler) getDislinktUserIdClaimFromJobOffersAPIToken(jobOffersAPIToken string) (string, error) {
+	token, err := jwt.Parse(jobOffersAPIToken, func(token *jwt.Token) (interface{}, error) {
+		if _, isSigningMethodValid := token.Method.(*jwt.SigningMethodHMAC); !isSigningMethodValid {
+			return nil, fmt.Errorf("invalid signing method")
+		}
+
+		audience := "billing.jwtgo.io"
+		isAudienceValid := token.Claims.(jwt.MapClaims).VerifyAudience(audience, false)
+		if !isAudienceValid {
+			return nil, fmt.Errorf("invalid audience")
+		}
+
+		issuer := "jwtgo.io"
+		isIssuerValid := token.Claims.(jwt.MapClaims).VerifyIssuer(issuer, false)
+		if !isIssuerValid {
+			return nil, fmt.Errorf("invalid issuer")
+		}
+
+		return []byte(os.Getenv("SECRET_FOR_JOB_OFFERS_API_TOKEN")), nil
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+	if token.Valid {
+		return token.Claims.(jwt.MapClaims)["dislinktUserId"].(string), nil
+	}
+
+	return "", fmt.Errorf("job offers api token is not valid")
+}
+
+func (handler *PostJobHandler) hasUserGeneratedJobOffersAPIToken(jobOffersAPITokenStatusRequest *domain.JobOffersAPITokenStatusRequest) error {
+	userClient := services.NewUserClient(handler.userClientAddress)
+
+	userResponse, err := userClient.Get(context.TODO(), &userPb.GetRequest{Id: jobOffersAPITokenStatusRequest.UserId})
+	if err != nil {
+		return err
+	}
+
+	if userResponse.User.JobOffersAPIToken != "" {
+		jobOffersAPITokenStatusRequest.HasGeneratedToken = true
+	} else {
+		jobOffersAPITokenStatusRequest.HasGeneratedToken = false
+	}
+
+	return nil
+}
+
+func (handler *PostJobHandler) createJob(jobProtobufObject *jobPb.Job) error {
+	jobClient := services.NewJobClient(handler.jobClientAddress)
+
+	addResponse, err := jobClient.Add(context.TODO(), &jobPb.AddRequest{Job: jobProtobufObject})
+	if err != nil {
+		return err
+	} else if addResponse.Success != "success" {
+		return fmt.Errorf("database already contains job with given id")
+	}
+
+	return nil
+}
