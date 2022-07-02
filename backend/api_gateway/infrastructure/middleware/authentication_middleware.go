7cc440d49d9b7e60be0d2bafacc45d09e643c496
Darijan Mićić <darijan.micic10@gmail.com>
2022-07-02 15:56:07 +0200

    feat: change some methods to unprotected ones

--- backend/api_gateway/infrastructure/middleware/authentication_middleware.go
+++ backend/api_gateway/infrastructure/middleware/authentication_middleware.go
@@ -85,17 +85,49 @@ func IsAuthenticated(handler *runtime.ServeMux) http.HandlerFunc {
 
 func isAProtectedRoute(method, path string) bool {
 	isPathToPostsOfPublicUser, _ := regexp.MatchString("/user/[0-9a-f]{24}/public", path)
-	isPathToUserProfile, _ := regexp.MatchString("/user/[0-9a-f]{24}", path)
-	pathToJob, _ := regexp.MatchString("/job/[0-9a-f]+", path)
+
+	isPathToJob, _ := regexp.MatchString("/job/[0-9a-f]+", path)
 	pathToAllJobs := "/job/jobs"
 	pathToJobAdding := "/job"
+	pathToJobEdit := "/job/editJob"
+
+	isPathToPostFromUser, _ := regexp.MatchString("/user/[0-9a-f]+/post/[0-9a-f]+", path)
+	isPathToAllPostsFromUser, _ := regexp.MatchString("/user/[0-9a-f]+/post", path)
+	isPathToCommentsOfPost, _ := regexp.MatchString("/user/[0-9a-f]+/post/[0-9a-f]+/comment", path)
+	isPathToLikeOrDislikeOfPost, _ :=
+		regexp.MatchString("/user/[0-9a-f]+/post/[0-9a-f]+/liked_or_disliked_by/(dis)?like", path)
+
+	isPathToUser, _ := regexp.MatchString("/user/[0-9a-f]+", path)
+	pathToAllUsers := "/user"
+	isPathToSearchOfJobsByUser, _ := regexp.MatchString("/job/searchByUser/[0-9a-f]+", path)
+	isPathToSearchOfJobsByDescription, _ :=
+		regexp.MatchString("/job/searchByDescription/[0-9a-f]+", path)
+	isPathToSearchOfJobsByPosition, _ :=
+		regexp.MatchString("/job/searchByPosition/[0-9a-f]+", path)
+	isPathToSearchOfJobsByRequirements, _ :=
+		regexp.MatchString("/job/searchByRequirements/[0-9a-f]+", path)
+	isPathToCheckOfPrivacyOfUser, _ := regexp.MatchString("/user/[0-9a-f]+/is-private", path)
+	pathToIdsOfAllPublicUsers := "/user/ids-of-all-public-users"
+	isPathToGenerationOfJobOffersAPIToken, _ :=
+		regexp.MatchString("/user/[0-9a-f]+/generate-job-offers-api-token", path)
+
 	switch method {
-	case "GET":
-		if isPathToPostsOfPublicUser || path == "/post/public" || path == "/user/public" || isPathToUserProfile || pathToJob || path == pathToAllJobs {
+	case http.MethodGet:
+		if isPathToPostsOfPublicUser || path == "/post/public" || path == "/user/public" || isPathToJob ||
+			path == pathToAllJobs || isPathToSearchOfJobsByUser || isPathToSearchOfJobsByDescription ||
+			isPathToSearchOfJobsByPosition || isPathToSearchOfJobsByRequirements || isPathToPostFromUser ||
+			isPathToAllPostsFromUser || isPathToCommentsOfPost || isPathToUser || path == pathToAllUsers ||
+			isPathToCheckOfPrivacyOfUser || path == pathToIdsOfAllPublicUsers ||
+			isPathToGenerationOfJobOffersAPIToken{
 			return false
 		}
-	case "POST":
-		if path == pathToJobAdding || path == "/user/register" || path == "/user/login" || path == "/user/search" {
+	case http.MethodPost:
+		if path == pathToJobAdding || path == "/user/register" || path == "/user/login" ||
+			path == "/user/search" || isPathToCommentsOfPost {
+			return false
+		}
+	case http.MethodPut:
+		if path == pathToJobEdit || isPathToLikeOrDislikeOfPost {
 			return false
 		}
 	}
@@ -109,48 +141,9 @@ func isUserAuthorizedToAccessRoute(authorizationDeterminingData AuthorizationDet
 	method := authorizationDeterminingData.Method
 	path := authorizationDeterminingData.Path
 
-	pathToSearchOfJobsByUser, _ := regexp.MatchString("/job/searchByUser/[0-9a-f]+", path)
-	pathToSearchOfJobsByDescription, _ :=
-		regexp.MatchString("/job/searchByDescription/[0-9a-f]+", path)
-	pathToSearchOfJobsByPosition, _ :=
-		regexp.MatchString("/job/searchByPosition/[0-9a-f]+", path)
-	pathToSearchOfJobsByRequirements, _ :=
-		regexp.MatchString("/job/searchByRequirements/[0-9a-f]+", path)
-	pathToJobEdit := "/job/editJob"
-
-	pathToPostFromUser, _ := regexp.MatchString("/user/[0-9a-f]+/post/[0-9a-f]+", path)
 	pathToAllPostsFromUser, _ := regexp.MatchString("/user/[0-9a-f]+/post", path)
-	pathToCommentsOfPost, _ := regexp.MatchString("/user/[0-9a-f]+/post/[0-9a-f]+/comment", path)
-	pathToLikeOrDislikeOfPost, _ :=
-		regexp.MatchString("/user/[0-9a-f]+/post/[0-9a-f]+/liked_or_disliked_by/(dis)?like", path)
 
 	pathToUser, _ := regexp.MatchString("/user/[0-9a-f]+", path)
-	pathToAllUsers := "/user"
-	pathToCheckOfPrivacyOfUser, _ := regexp.MatchString("/user/[0-9a-f]+/is-private", path)
-	pathToIdsOfAllPublicUsers := "/user/ids-of-all-public-users"
-	pathToGenerationOfJobOffersAPIToken, _ :=
-		regexp.MatchString("/user/[0-9a-f]+/generate-job-offers-api-token", path)
-
-	if (pathToSearchOfJobsByUser || pathToSearchOfJobsByDescription || pathToSearchOfJobsByPosition ||
-		pathToSearchOfJobsByRequirements) && method == http.MethodGet {
-		return true
-	}
-
-	if path == pathToJobEdit && method == http.MethodPut {
-		if userRole == Administrator {
-			return true
-		}
-
-		return false
-	}
-
-	if pathToPostFromUser && method == http.MethodGet {
-		return true
-	}
-
-	if pathToAllPostsFromUser && method == http.MethodGet {
-		return true
-	}
 
 	if pathToAllPostsFromUser && method == http.MethodPost {
 		userIdInPath := strings.TrimPrefix(path, "/user/")
@@ -161,22 +154,6 @@ func isUserAuthorizedToAccessRoute(authorizationDeterminingData AuthorizationDet
 		}
 
 		return false
-	}
-
-	if pathToCommentsOfPost && method == http.MethodGet {
-		return true
-	}
-
-	if pathToCommentsOfPost && method == http.MethodPost {
-		return true
-	}
-
-	if pathToLikeOrDislikeOfPost && method == http.MethodPut {
-		return true
-	}
-
-	if pathToUser && method == http.MethodGet {
-		return true
 	}
 
 	if pathToUser && method == http.MethodPut {
@@ -190,22 +167,6 @@ func isUserAuthorizedToAccessRoute(authorizationDeterminingData AuthorizationDet
 		}
 
 		return false
-	}
-
-	if path == pathToAllUsers && method == http.MethodGet {
-		return true
-	}
-
-	if pathToCheckOfPrivacyOfUser && method == http.MethodGet {
-		return true
-	}
-
-	if path == pathToIdsOfAllPublicUsers && method == http.MethodGet {
-		return true
-	}
-
-	if pathToGenerationOfJobOffersAPIToken && method == http.MethodGet {
-		return true
 	}
 
 	return false
