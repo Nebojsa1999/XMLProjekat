f189a4de78afd4b92b61dfb7bfe8d4e979272e88
Darijan Mićić <darijan.micic10@gmail.com>
2022-07-09 15:44:54 +0200

    feat: add config of missing 3 layers to server

--- agent-app-backend/startup/server.go
+++ agent-app-backend/startup/server.go
@@ -34,19 +34,35 @@ func (server *Server) Start() {
 	userStore := server.initUserStore(mongoClient)
 	companyStore := server.initCompanyStore(mongoClient)
 	companyRegistrationRequestStore := server.initCompanyRegistrationRequestStore(mongoClient)
+	jobStore := server.initJobStore(mongoClient)
+	commentStore := server.initCommentStore(mongoClient)
+	wageStore := server.initWageStore(mongoClient)
+	interviewStore := server.initInterviewStore(mongoClient)
 
 	userService := server.initUserService(userStore)
 	companyService := server.initCompanyService(companyStore)
 	companyRegistrationRequestService := server.initCompanyRegistrationRequestService(companyRegistrationRequestStore)
+	jobService := server.initJobService(jobStore)
+	commentService := server.initCommentService(commentStore)
+	wageService := server.initWageService(wageStore)
+	interviewService := server.initInterviewService(interviewStore)
 
 	userHandler := server.initUserHandler(userService)
 	companyHandler := server.initCompanyHandler(companyService)
 	companyRegistrationRequestHandler := server.initCompanyRegistrationRequestHandler(companyRegistrationRequestService)
+	jobHandler := server.initJobHandler(jobService)
+	commentHandler := server.initCommentHandler(commentService)
+	wageHandler := server.initWageHandler(wageService)
+	interviewHandler := server.initInterviewHandler(interviewService)
 
 	server.startHttpServer(cfg.Handlers{
 		UserHandler:                       userHandler,
 		CompanyHandler:                    companyHandler,
 		CompanyRegistrationRequestHandler: companyRegistrationRequestHandler,
+		JobHandler:                        jobHandler,
+		CommentHandler:                    commentHandler,
+		WageHandler:                       wageHandler,
+		InterviewHandler:                  interviewHandler,
 	})
 }
 
@@ -110,6 +126,74 @@ func (server *Server) initCompanyRegistrationRequestStore(client *mongo.Client) 
 	return store
 }
 
+func (server *Server) initJobStore(client *mongo.Client) domain.JobStore {
+	store := persistence.NewJobMongoDBStore(client)
+	_, err := store.DeleteAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, job := range jobs {
+		_, err := store.CreateNewJob(job)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	return store
+}
+
+func (server *Server) initCommentStore(client *mongo.Client) domain.CommentStore {
+	store := persistence.NewCommentMongoDBStore(client)
+	_, err := store.DeleteAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, comment := range comments {
+		_, err := store.CreateNewComment(comment)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	return store
+}
+
+func (server *Server) initWageStore(client *mongo.Client) domain.WageStore {
+	store := persistence.NewWageMongoDBStore(client)
+	_, err := store.DeleteAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, wage := range wages {
+		_, err := store.CreateNewWage(wage)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	return store
+}
+
+func (server *Server) initInterviewStore(client *mongo.Client) domain.InterviewStore {
+	store := persistence.NewInterviewMongoDBStore(client)
+	_, err := store.DeleteAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, interview := range interviews {
+		_, err := store.CreateNewInterview(interview)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	return store
+}
+
 func (server *Server) initUserService(store domain.UserStore) *application.UserService {
 	return application.NewUserService(store)
 }
@@ -122,6 +206,22 @@ func (server *Server) initCompanyRegistrationRequestService(store domain.Company
 	return application.NewCompanyRegistrationRequestService(store)
 }
 
+func (server *Server) initJobService(store domain.JobStore) *application.JobService {
+	return application.NewJobService(store)
+}
+
+func (server *Server) initCommentService(store domain.CommentStore) *application.CommentService {
+	return application.NewCommentService(store)
+}
+
+func (server *Server) initWageService(store domain.WageStore) *application.WageService {
+	return application.NewWageService(store)
+}
+
+func (server *Server) initInterviewService(store domain.InterviewStore) *application.InterviewService {
+	return application.NewInterviewService(store)
+}
+
 func (server *Server) initUserHandler(service *application.UserService) *api.UserHandler {
 	return api.NewUserHandler(service)
 }
@@ -132,6 +232,22 @@ func (server *Server) initCompanyHandler(service *application.CompanyService) *a
 
 func (server *Server) initCompanyRegistrationRequestHandler(service *application.CompanyRegistrationRequestService) *api.CompanyRegistrationRequestHandler {
 	return api.NewCompanyRegistrationRequestHandler(service)
+}
+
+func (server *Server) initJobHandler(service *application.JobService) *api.JobHandler {
+	return api.NewJobHandler(service)
+}
+
+func (server *Server) initCommentHandler(service *application.CommentService) *api.CommentHandler {
+	return api.NewCommentHandler(service)
+}
+
+func (server *Server) initWageHandler(service *application.WageService) *api.WageHandler {
+	return api.NewWageHandler(service)
+}
+
+func (server *Server) initInterviewHandler(service *application.InterviewService) *api.InterviewHandler {
+	return api.NewInterviewHandler(service)
 }
 
 func (server *Server) startHttpServer(handlers cfg.Handlers) {
